90b910565d26d971dc6279fc10b6a47df43f1052
NiiOdartei7 <43499683+NiiOdartei7@users.noreply.github.com>
2020-07-09 18:08:55 +0000

    Update Assignment0.py

--- Assignment0.py
+++ Assignment0.py
@@ -3,16 +3,16 @@
 #It takes two inputs
 #The temeperature and the unit it is in
 
-unit = ('Celsuis')
+unit = ('Celsius')
 unit2 = ('Fahrenheit')
 temp = eval(input('Enter a temperature: ',))
-user = input('Celsuis, Fahrenheit: ',)
+user = input('Celsius, Fahrenheit: ',)
 
 if unit == user:
             print('In Fahrenheit: ', (9/5*temp)+32)
             
 elif unit2 == user:
-            print('In Celsuis: ', 5/9*(temp-32))
+            print('In Celsius: ', 5/9*(temp-32))
             
 else:
     print("That is not a unit")
