d98df12fa14bc02628732fea4a469432b0806116
NiiOdartei7 <43499683+NiiOdartei7@users.noreply.github.com>
2020-07-09 18:16:27 +0000

    Update Assignment0.py

--- Assignment0.py
+++ Assignment0.py
@@ -2,17 +2,19 @@
 #This program converts Celsius to Fahrenheit and viceversa"
 #It takes two inputs
 #The temeperature and the unit it is in
+#The output is the converted temperature value
+
 
 unit = ('Celsius')
 unit2 = ('Fahrenheit')
 temp = eval(input('Enter a temperature: ',))
-user = input('Celsius, Fahrenheit: ',)
+user = input('Is it in Celsius or Fahrenheit (Enter the unit only): ',)
 
 if unit == user:
-            print('In Fahrenheit: ', (9/5*temp)+32)
+            print('In Fahrenheit it is: ', (9/5*temp)+32)
             
 elif unit2 == user:
-            print('In Celsius: ', 5/9*(temp-32))
+            print('In Celsius it is: ', 5/9*(temp-32))
             
 else:
     print("That is not a unit")
